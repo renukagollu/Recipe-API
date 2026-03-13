96376d08cc8ca6976fc41640d8308de0bbbff28b
renukagollu <87572643+renukagollu@users.noreply.github.com>
2026-03-13 21:21:47 +0100

    Delete recipe_api/app directory

--- recipe_api/app/db/base.py
+++
@@ -1,6 +0,0 @@
-"""This module imports all the database models so that they can be easily accessed from other parts of the
-application."""
-
-from app.db.models import Ingredient, Recipe, RecipeIngredient
-
-__all__ = ["Recipe", "Ingredient", "RecipeIngredient"]
